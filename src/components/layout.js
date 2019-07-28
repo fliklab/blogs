d477342a1290fb6a579c0706c0c58ff67fe714dc
seonghunjeong <jsh852@gmail.com>
2019-07-28 18:25:39 +0900

    add graphql playground

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,65 +1,61 @@
-import React from "react"
 import { css } from "@emotion/core"
+import { graphql, Link, useStaticQuery } from "gatsby"
+import React from "react"
 import { rhythm } from "../utils/typography"
-import { useStaticQuery, Link, graphql } from "gatsby"
 
-
-
-export default ({children}) => {
-    const data = useStaticQuery(
-        graphql`
-          query {
-            site {
-              siteMetadata {
-                title
-              }
-            }
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
           }
-        `
-      )
-      return (
-  <div
-    css={css`
-      margin: 0 auto;
-      max-width: 700px;
-      padding: $(rhythm(2));
-      padding-top: $(rhythm(1.5));
-    `}
-  >
-    <Link to={"/"}>
-      <h3
-        css={css`
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: $(rhythm(2));
+        padding-top: $(rhythm(1.5));
+      `}
+    >
+      <Link to={"/"}>
+        <h3
+          css={css`
         margin-top: 10px};
 
         margin-bottom: ${rhythm(2)};
           display: inline-block;
           font-style: normal;
         `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={"/about/"}
+        css={css`
+          margin: 0.2rem;
+          float: right;
+        `}
       >
-        {data.site.siteMetadata.title}
-
-      </h3>
-    </Link>
-    <Link
-      to={"/about/"}
-      css={css`
-        margin: 0.2rem;
-        float: right;
-      `}
-    >
-      About
-    </Link>
-
-    <Link
-      to={"/home/"}
-      css={css`
-        margin: 0.2rem;
-        float: right;
-      `}
-    >
-      Contact
-    </Link>
-    {children}
-  </div>
-      )
+        About
+      </Link>
+      <Link
+        to={"/contact/"}
+        css={css`
+          margin: 0.2rem;
+          float: right;
+        `}
+      >
+        Contact
+      </Link>
+      {children}
+    </div>
+  )
 }
